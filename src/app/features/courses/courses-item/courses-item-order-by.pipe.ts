5eaa84ad063d2eccbb27906747e90631ffaeb8e9
sergii.azizov <sergii.azizov@aperturegroup.com>
2017-04-03 17:05:25 +0300

    HM5: done

--- src/app/features/courses/courses-item/courses-item-order-by.pipe.ts
+++ src/app/features/courses/courses-item/courses-item-order-by.pipe.ts
@@ -4,6 +4,6 @@ import { Course } from '../shared/interfaces/course.interface'
 @Pipe({name: 'coursesOrderBy'})
 export class CoursesOrderByPipe implements PipeTransform {
   transform(origin: Course[], field: string): Course[] {
-    return origin.sort((a: Course, b: Course): boolean => (a[field] < b[field]));
+    return origin.sort((a: Course, b: Course): number => a[field] < b[field] ? 1 : -1);
   }
 }
